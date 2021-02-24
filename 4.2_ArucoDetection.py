12b109eb9da89118cee21088964407ec996010cd
SkunkHeadedPunk <jack.woolery.94@gmail.com>
2021-02-23 22:58:07 -0700

    quadrant detection completed

--- 4.2_ArucoDetection.py
+++ 4.2_ArucoDetection.py
@@ -15,6 +15,7 @@ See 'SEED_Assignment_Document.PDF' for more details and instructions.
 __author__ = "Jack Woolery"
 __email__ = "lwoolery@mines.edu"
 
+from picamera.array import PiRGBArray
 from picamera import PiCamera
 import picamera
 import time
@@ -35,49 +36,75 @@ def detect_marker(img):
                                          ids=ids,
                                          borderColor=(0, 0, 255)
                                          )
+            sum_x = 0
+            sum_y = 0
             for i in range(4):
                 sum_x = sum_x + corners[0][0][i, 0]
                 sum_y = sum_y + corners[0][0][i, 1]
             ave_x = sum_x / 4
             ave_y = sum_y / 4
             loc.append([ave_x, ave_y])
+            
+        quad, img_marked = get_quadrant(loc, img)
 
     if ids is None:
         print("Marker not detected")
+        quad = "0"
+        img_marked = img
+
+    #cv.putText(img, quad, (200, 200), cv.FONT_HERSHEY_SIMPLEX, 4, (0, 0, 255),
+               #2, cv.LINE_AA)
+    
         
-    return loc, img
+    return img_marked
 
 def get_quadrant(loc, img):
+    quad = 0
     cx = img.shape[1] / 2
     cy = img.shape[0] / 2
 
-    print("loc: ", loc)
     loc = loc[0]
-    print("loc[0]: ", loc)
+    x = loc[0]
+    y = loc[1]
 
-    return quad, output
+    if x >= cx and y < cy:
+        quad = "1"
 
-def main():
+    if x < cx and y < cy:
+        quad = "2"
 
+    if x < cx and y >= cy:
+        quad = "3"
+
+    if x >= cx and y >= cy:
+        quad = "4"
+
+    cv.putText(img, quad, (0, 25), cv.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
+    print("Quadrant: ", quad)
+
+    return quad, img
+
+if __name__ == '__main__':
+    camera = PiCamera()
     x = input("Enter the number of seconds you'd like to take photos for: ")
     # Change user input to int to set time for capture stream
     x = int(x)
     t_minus_x_seconds = time.time() + x
 
-    with picamera.PiCamera() as camera:
-        camera.resolution = (320, 240)
-        camera.framerate = 24
-        time.sleep(2)
+    with picamera.array.PiRGBArray(camera) as output:
         while time.time() < t_minus_x_seconds:
-            img = np.empty((240 * 320 * 3,), dtype=np.uint8)
-            camera.capture(img, 'bgr')
-            img = img.reshape((240, 320, 3))
+            camera.capture(output, format="bgr")
+            img = output.array
 
             gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-            loc, img_markers = detect_marker(gray_img)
+            scale = 0.5
+            resize_img = cv.resize(gray_img, None, fx=scale, fy=scale,
+                                   interpolation = cv.INTER_CUBIC)
 
-            quad, output = get_quadrant(loc, img_markers)
+            img_markers = detect_marker(resize_img)
+
+            
 
             cv.imshow("Markers Stream", img_markers)
             cv.waitKey(500)
